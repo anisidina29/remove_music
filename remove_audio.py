652538de181b16a0bebd6c36cc4e27f6f210c0bb
NguyenDuyThanhB1909984 <101155685+NguyenDuyThanhB1909984@users.noreply.github.com>
2024-06-19 19:58:47 +0700

    Update remove_audio.py

--- remove_audio.py
+++ remove_audio.py
@@ -4,10 +4,10 @@ import shutil
 import sys
 
 def process_video(video_path, output_dir, final_output_dir):
-    print(video_path)
+    print(f"Processing video: {video_path}")
     # Tách âm thanh bằng demucs
-    command = f"python -m demucs.separate --out {output_dir} {video_path}"
-    subprocess.run(command, shell=True)
+    command = f"python -m demucs.separate --out \"{output_dir}\" \"{video_path}\""
+    subprocess.run(command, shell=True, check=True)
 
     # Đường dẫn tới tệp âm thanh giọng hát đã tách
     vocals_path = os.path.join(output_dir, "htdemucs", os.path.basename(video_path).replace(".mp4", ""), "vocals.wav")
@@ -16,11 +16,11 @@ def process_video(video_path, output_dir, final_output_dir):
     temp_video_path = f"{video_path[0:-4]}_temp.mp4"
 
     # Tách âm thanh khỏi video và lưu lại video không có âm thanh
-    subprocess.run(f"ffmpeg -i {video_path} -c copy -an {temp_video_path}", shell=True)
+    subprocess.run(f"ffmpeg -i \"{video_path}\" -c copy -an \"{temp_video_path}\"", shell=True, check=True)
 
     # Thay âm thanh của video bằng âm thanh giọng hát đã tách và lưu video mới
     output_video_path = f"{video_path[0:-4]}_without_music.mp4"
-    subprocess.run(f"ffmpeg -i {temp_video_path} -i {vocals_path} -c:v copy -c:a aac -strict experimental {output_video_path}", shell=True)
+    subprocess.run(f"ffmpeg -i \"{temp_video_path}\" -i \"{vocals_path}\" -c:v copy -c:a aac -strict experimental \"{output_video_path}\"", shell=True, check=True)
 
     # Di chuyển video đã xử lý vào thư mục cuối cùng
     final_video_path = os.path.join(final_output_dir, os.path.basename(video_path).replace(".mp4", "_without_music.mp4"))

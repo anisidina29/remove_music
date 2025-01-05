07371bdd36242615295f418b269f287519ddb3ab
Chip Huyen <thanh6301@gmail.com>
2025-01-05 21:18:16 +0700

    Update watch_dailymotion.py

--- watch_dailymotion.py
+++ watch_dailymotion.py
@@ -108,7 +108,7 @@ num_threads = 4
 
 # Khởi tạo và chạy nhiều threads cho các trình duyệt khác nhau
 threads = []
-browser_types = ['chrome', 'edge','chrome', 'edge']
+browser_types = ['chrome', 'chrome','chrome', 'chrome','chrome']
 
 for i in range(num_threads):
     browser_type = browser_types[i % len(browser_types)]  # Alternate browsers

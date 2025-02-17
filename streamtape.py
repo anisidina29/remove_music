98ebce39c4634e97d78d505f8e325039e9830274
Chip Huyen <thanh6301@gmail.com>
2025-02-18 00:37:30 +0700

    Update streamtape.py

--- streamtape.py
+++ streamtape.py
@@ -45,7 +45,7 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
 
-driver = uc.Chrome(service=Service(ChromeDriverManager(version="132.0.6834.84").install()), options=options)
+driver = uc.Chrome(options=options)
 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
 # Di chuyển chuột ngẫu nhiên
